403563e3fad3914cf6c0ccb2c25471e65eaafa69
yancey <lyf_0815@126.com>
2024-03-18 16:24:58 +0800

    Update position-history.js

--- position-history.js
+++ position-history.js
@@ -6,9 +6,9 @@ if (url.indexOf('bapi/composite/v1/private/bigdata/finance/futures/query-positio
     let data = body.data;
     if (data.positionHistoryItemList != null) {
        data.positionHistoryItemList.forEach(item => {
-           item.closed_volume = item.closed_volume * 10;
-           item.closing_pnl = item.closing_pnl * 10;
-           item.max_open_interest = item.max_open_interest * 10;
+           item.closed_volume = item.closed_volume * 1000;
+           item.closing_pnl = item.closing_pnl * 1000;
+           item.max_open_interest = item.max_open_interest * 1000;
        })
     }
     
