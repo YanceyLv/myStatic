bcdbb84f4a7186085065a241bc9617f86d1a6520
yancey <lyf_0815@126.com>
2024-03-18 16:45:47 +0800

    Update position-history.js

--- position-history.js
+++ position-history.js
@@ -2,17 +2,45 @@
 let url = $request.url
 let body = JSON.parse($response.body)
 
+let multiple = 1000;
 if (url.indexOf('bapi/composite/v1/private/bigdata/finance/futures/query-position-history') !== -1) {
     let data = body.data;
     if (data.positionHistoryItemList != null) {
-       data.positionHistoryItemList.forEach(item => {
-           item.closed_volume = item.closed_volume * 1000;
-           item.closing_pnl = item.closing_pnl * 1000;
-           item.max_open_interest = item.max_open_interest * 1000;
-       })
+        data.positionHistoryItemList.forEach(item => {
+            item.closed_volume = item.closed_volume * multiple;
+            item.closing_pnl = item.closing_pnl * multiple;
+            item.max_open_interest = item.max_open_interest * multiple;
+        })
     }
-    
+
     $done({ body: JSON.stringify(body) })
-}else {
-  $done({})
+}else if (url.indexOf('bapi/futures/v1/private/future/user-data/trade-history') !== -1) {
+    let data = body.data;
+    if (data != null) {
+        data.forEach(item => {
+            item.fee = item.fee * multiple;
+            item.qty = item.qty * multiple;
+            item.totalQuota = item.totalQuota * multiple;
+        })
+    }
+    $done({ body: JSON.stringify(body) })
+}else if (url.indexOf('bapi/futures/v1/private/future/user-data/transaction-history') !== -1) {
+    let data = body.data;
+    if (data != null) {
+        data.forEach(item => {
+            item.balanceDelta = item.balanceDelta * multiple;
+            item.balanceDeltaStr = item.balanceDelta + "";
+        })
+    }
+    $done({ body: JSON.stringify(body) })
+}else if (url.indexOf('bapi/futures/v1/private/future/order/order-history') !== -1) {
+    let data = body.data;
+    if (data != null) {
+        data.forEach(item => {
+            item.delegateMoney = item.delegateMoney * multiple;
+        })
+    }
+    $done({ body: JSON.stringify(body) })
+}else{
+    $done({})
 }
